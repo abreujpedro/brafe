5b2e86a230d15524e9b55216236684ceccc43526
João Pedro Della Bianca Abreu <jp.dellabianca@gmail.com>
2021-05-13 18:24:12 -0300

    Adicionadas animações js

---
+++ assets/js/menuMobile.js
@@ -0,0 +1,36 @@
+export default function btnMobile (){
+    const btn = document.querySelector('.mobile-content');
+    const barras = document.querySelector('.btn-mobile');
+    const menuNav = document.querySelector(".menu-nav");
+    const itens = document.querySelectorAll(".menu-nav ul li a[href^='#']");
+    
+    function toggleScroll () {
+        if (menuNav.classList.contains("ativado")){
+            document.documentElement.style.overflow = 'hidden';
+            document.body.scroll = "no";
+        }else{
+            document.documentElement.style.overflow = 'auto';
+            document.body.scroll = "yes";
+        }
+    }
+
+    function callBack(){
+        menuNav.classList.toggle("ativado");
+        btn.classList.toggle("at");
+        barras.classList.toggle("ativ");
+        toggleScroll();
+    }
+
+
+    function removeBack () {
+        menuNav.classList.remove("ativado");
+        btn.classList.remove("at");
+        barras.classList.remove("ativ");
+        toggleScroll();
+    }
+
+    itens.forEach((i) =>{
+        i.addEventListener("click", removeBack);
+    })
+    btn.addEventListener("click", callBack);
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,11 @@ import scrollAnim from "./scrollAnim.js";
 import scrollBtn from "./scrollBtn.js";
 import typing from "./type.js";
 import modal from "./modal.js";
+import menuMobile from "./menuMobile.js"
 
 scroll();
 scrollAnim();
 scrollBtn();
 typing();
 modal();
+menuMobile();
